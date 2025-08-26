6ce47fda2c7ee6c0cb6b34f6a3a7dfe8273b8333
Dani <daniele.ddsp@gmail.com>
2025-08-25 22:56:21 -0300

    criando casos de testes automatizados

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -4,6 +4,7 @@ package calculadora;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Assertions;
@@ -21,7 +22,7 @@ public class CalculadoraTest {
 		calc = new Calculadora();
 	}
 	
-	@DisplayName("Testa a soma de dois números")
+	@DisplayName("Testa a soma de dois nÃºmeros")
 	@Test
 	public void testSomaDoisNumeros() {
 		int soma = calc.soma(4, 5);		
@@ -38,16 +39,34 @@ public class CalculadoraTest {
 	public void testDivisaoPorZero() {
 		try {
 			int divisao = calc.divisao(8, 0);
-			fail("Exceção não lançada");
+			fail("ExceÃ§Ã£o nÃ£o lanÃ§ada");
 		}catch (ArithmeticException e) {
 			assertEquals("/ by zero", e.getMessage());
 		}		
 	}
-	
+		
 	@Test
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
+	}
+	
+	@Test
+	public void testSomatoria() {
+		assertEquals(10, calc.somatoria(4));
+	}
+	
+	@Test
+	public void testEhPositivo() {
+		assertTrue(calc.ehPositivo(10));
+		assertFalse(calc.ehPositivo(-1));
+	}	
+	
+	@Test
+	public void testCompara() {
+		assertEquals(0, calc.compara(15, 15)); // retorna 0 se a==b
+		assertEquals(1, calc.compara(15, 4)); // retorna 1 se a>b
+		assertEquals(-1, calc.compara(4, 8)); // retorna-1 se a<b
 	}
 
 }
