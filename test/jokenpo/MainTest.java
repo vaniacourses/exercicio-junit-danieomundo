34f388c1fcecdae82eb9cf37fefb52aa81112381
Dani <daniele.ddsp@gmail.com>
2025-09-01 19:26:41 -0300

    Create MainTest.java

---
+++ test/jokenpo/MainTest.java
@@ -0,0 +1,18 @@
+package jokenpo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+	
+	private Main jokenpo;
+
+	//@BeforeEach
+	//public void 
+  // obs: ainda não finalizado.
+
+  
+}
